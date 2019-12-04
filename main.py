58fa2af81a8e3c9e66a55ab8956326732a9b754d
Eduardo Imery <ejwimery@gmail.com>
2019-12-04 10:05:42 -0600

    Creating the conection to DB and setting up the file

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import sqlite3 as lite
 from sqlite3 import Error
 import tkinter as tk
 from glob import glob
+
+def create(obj):
+    db = obj.e.get()
+    
+    if db[-3] == ".db":
+        pass
+    else:
+        db = db + ".db"
+    try:
+        conn = lite.connect(db)
+        return conn 
