0d297b644821f2a90aacdb893b18283e6b12d171
Eduardo Imery <ejwimery@gmail.com>
2019-12-04 12:05:06 -0600

    refactoring the code

--- main.py
+++ main.py
@@ -6,27 +6,61 @@ from glob import glob
  
 class App:
     """ Creando el widget para la wentada"""
-
-    def __init__(self, root):
+    def __int__(self, root):
+        self.fields =[]
         self.root = root
+        self.label()
+        self.entry()
+        self.button()
+        self.listbox()
+        self.db_name_widgets()
+        self.tb_name_widgets()
+        self.fields_widgets()
+        self.btn_create_table()
+
+    def label(self):
         #creating the label for name DB
         self.l = tk.Label(self.root, text="Crear DB [inserte nombre]")
         self.l.pack()
-        #Setting the var
-        self.e_string_var = tk.StringVar()
-        self.e = tk.Entry(self.root, textvariable=self.e_string_var)
+
+    def entry(self):
+        self.db = tk.StringVar()
+        self.e = tk.Entry(self.root, textvariable=self.db)
         self.e.pack()
+
+    def button(self):
         #Creating the button for crate DB
         self.b = tk.Button(
 			self.root,
 			text="Crear DB",
 			command= lambda: self.mk_db())
         self.b.pack()
+
+    def listbox(self):
         #Setting the list box for show the bd created
         self.lb = tk.Listbox(self.root)
         self.lb.pack()
         self.show_db()
-    #funtion to create the DB
+
+    def db_name_widgets(self):
+        self.lbdname = tk.Label(
+            self.root,
+            text ="Inserta DB name"
+        )
+        self.lbdname.pack()
+        self.dbn = tk.StringVar()
+        self.edb = tk.Entry(
+            self.root,
+            textvariable=self.dbn)
+        self.edb.pack()
+
+    def tb_name_widgets(self):
+        self.ltbname = tk.Label(
+            self.root,
+            text="Ingresa nombre para Tabla")
+        self.ltbname.pack()
+        
+
     def mk_db(self):
         db = self.e.get()
         if db.endswith(".db"):
